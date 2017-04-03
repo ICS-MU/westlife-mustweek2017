a04b837687ae529c873edd36e5af5d365a6751bc
Tomas Racek <tom@krab1k.net>
2017-04-03 19:14:19 +0000

    add worker binaries as resources

--- solved/scripts/saxs-worker/create.sh
+++ solved/scripts/saxs-worker/create.sh
@@ -9,8 +9,6 @@ set -x
 PATH=/tmp/cloudify-ctx:$PATH
 
 yum -y -d1 install wget openmpi environment-modules
-# wget https://integrativemodeling.org/2.6.2/download/IMP-2.6.2-1.el7.centos.x86_64.rpm
-# yum -y -d1 localinstall IMP-2.6.2-1.el7.centos.x86_64.rpm
 
 imprpm=`ctx node properties imp_url`
 
@@ -18,12 +16,10 @@ ctx logger info "Downloading $imprpm"
 wget $imprpm
 yum -y -d1 localinstall `basename $imprpm`
 
-wget http://www.fi.muni.cz/~xracek/westlife/ensamble-fit -O /usr/local/bin/ensamble-fit
-wget http://www.fi.muni.cz/~xracek/westlife/foxs -O /usr/local/bin/foxs
-chmod +x /usr/local/bin/ensamble-fit
-chmod +x /usr/local/bin/foxs
-adduser saxs
+ctx download-resource resources/saxs-worker/ensamble-fit '@{"target_path": "/usr/local/bin/ensamble-fit"}'
+ctx download-resource resources/saxs-worker/foxs '@{"target_path": "/usr/local/bin/foxs"}'
 
+adduser saxs
 mkdir ~saxs/.ssh
 ctx download-resource resources/ssh/id_rsa_saxs '@{"target_path": "/tmp/id_rsa_saxs"}'
 mv /tmp/id_rsa_saxs ~saxs/.ssh/id_rsa
