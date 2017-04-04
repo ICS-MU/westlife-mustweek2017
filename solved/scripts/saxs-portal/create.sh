6a76a5b972626c42a01651e371059b6ba319a0fc
Tomas Racek <tom@krab1k.net>
2017-04-04 07:37:24 +0000

    Added SAXS portal

---
+++ solved/scripts/saxs-portal/create.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+if [ `id -u` -ne 0 ]; then
+	exec sudo -E /bin/bash -c "$0"
+fi
+
+set -x
+
+PATH=/tmp/cloudify-ctx:$PATH
+
+ctx logger info "Runnig at `hostname`"
+
+# Install necessary tools
+ctx logger info "Installing dependencies"
+yum -y -d1 install python-virtualenv wget python34
+
+# Setup apache for SAXS
+ctx logger info "Configuring Apache"
+rm -f /etc/httpd/conf.d/*.conf
+cat > /etc/httpd/conf.d/saxs.conf << EOF
+DocumentRoot	/var/www/saxs
+Alias /static /var/www/saxs/app/static
+ScriptAlias / /var/www/saxs/run.cgi/
+
+<Directory /var/www/saxs>
+    Options +ExecCGI
+    AddHandler cgi-script .cgi
+    SetEnv  HTTP_EPPN john.hacker@somewhere.com
+    SetEnv  HTTP_CN "John Hacker"
+    SetEnv  HTTP_MAIL john.hacker@somewhere.com
+</Directory>
+EOF
+
+adduser saxs
+usermod -G saxs,apache apache
+usermod -G saxs,apache saxs
+
+# Setup SAXS portal application
+ctx logger info "Setting up web interface"
+ctx download-resource resources/saxs-portal/saxs-portal.tar.gz '@{"target_path": "/tmp/saxs-portal.tar.gz"}'
+tar xvzf /tmp/saxs-portal.tar.gz -C /var/www
+chown -R apache:saxs /var/www/saxs
+cd /var/www/saxs
+virtualenv flask
+source flask/bin/activate
+pip install -r requirements.txt &>/dev/null
+
+# Setup SAXS portal database with one default user
+ctx logger info "Configuring database"
+mkdir -p /var/www/SaxsExperiments/1
+python /var/www/saxs/db_create.py
+sqlite3 /var/www/SaxsExperiments/app.db "insert into users values (1,'John Hacker','john.hacker@somewhere.com','john.hacker@somewhere.com',1,1);"
+
+sed -i 's/Group apache/Group saxs/' /etc/httpd/conf/httpd.conf
+chown -R apache:saxs /var/www/SaxsExperiments
+
+# Install server-side scripts
+ctx logger info "Setting up submission server"
+ctx download-resource resources/saxs-portal/saxs-server.tar.gz '@{"target_path": "/tmp/saxs-server.tar.gz"}'
+tar xvzf /tmp/saxs-server.tar.gz --no-overwrite-dir -C /
+
+# Setup syslog for SAXS
+echo "local7.* /var/log/saxs.log" > /etc/rsyslog.d/30-saxs.conf
+systemctl restart rsyslog
+
+# Restart apache
+systemctl restart httpd
