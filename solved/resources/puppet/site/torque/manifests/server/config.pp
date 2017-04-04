098649279adc5091b09283c66880ea0dbd214ced
Tomas Racek <tom@krab1k.net>
2017-04-04 08:05:45 +0000

    Added torque

---
+++ solved/resources/puppet/site/torque/manifests/server/config.pp
@@ -0,0 +1,20 @@
+class torque::server::config {
+  exec { 'timeout --preserve-status 5 pbs_server -f -t create -D':
+    path    => '/bin:/usr/bin:/sbin/:/usr/sbin',
+    creates => $::torque::server::serverdb_file,
+  }
+
+  # collect exported/virtual resources
+  Torque_node       <<| server_name == $::torque::server::server_name |>>
+  Torque_node        <| server_name == $::torque::server::server_name |>
+  Torque::Mom::Node <<| server_name == $::torque::server::server_name |>>
+  Torque::Mom::Node  <| server_name == $::torque::server::server_name |>
+
+  # create node resources from provided hash
+  if size($::torque::server::nodes) > 0 { #TODO: length()
+    ensure_resource(
+      'torque_node',
+      $::torque::server::nodes,
+      { 'ensure' => 'present', })
+  }
+}
